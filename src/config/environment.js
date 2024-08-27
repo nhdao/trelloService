a055e0de3ff35f4a008344e50cf52689fc7e811e
NHD <nguyenhoangdao1609@.gmail.com>
2024-08-27 21:16:50 +0700

    Add env and mongo connection

--- src/config/environment.js
+++ src/config/environment.js
@@ -3,3 +3,14 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+export const env = {
+  MONGO_URI: process.env.MONGO_URI,
+  DB_NAME: process.env.DB_NAME,
+  APP_HOST: process.env.APP_HOST,
+  APP_PORT: process.env.APP_PORT,
+  AUTHOR: process.env.AUTHOR
+}
--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -3,3 +3,37 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+const { MongoClient, ServerApiVersion } = require('mongodb')
+const { env } = require('./environment')
+
+let trelloDBInstance = null
+
+const mongoClientInstance = new MongoClient(env.MONGO_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+export const CONNECT_DB = async () => {
+  try {
+    await mongoClientInstance.connect()
+
+    trelloDBInstance = mongoClientInstance.db(env.DB_NAME)
+  } catch (err) {
+    console.log(err.message)
+  }
+}
+
+export const CLOSE_DB = async () => {
+  await mongoClientInstance.close()
+}
+
+export const GET_DB = () => {
+  if (!trelloDBInstance) {
+    throw new Error('Must connect to DB first')
+  }
+
+  return trelloDBInstance
+}
