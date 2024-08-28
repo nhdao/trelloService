c9ef928c0be95c11c5af49354033183333ccaefe
NHD <nguyenhoangdao1609@.gmail.com>
2024-08-28 08:58:54 +0700

    Add board controllers

--- src/routes/v1/boardRoutes.js
+++ src/routes/v1/boardRoutes.js
@@ -7,9 +7,10 @@ const express = require('express')
 const { StatusCodes } = require('http-status-codes')
 const router = express.Router()
 const { boardValidation } = require('./../../validations/boardValidation')
+const { boardController } = require('./../../controllers/boardControllers')
 
 router.route('/')
   .get()
-  .post(boardValidation.createNew)
+  .post(boardValidation.createNew, boardController.createNew)
 
 export const boardRoutes = router
